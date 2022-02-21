93ae41f89cebfdb2b581e243aaaadffc2f373fee
Anthony <antromano21@gmail.com>
2022-02-20 19:34:54 -0500

    update

--- db/seed.js
+++ db/seed.js
@@ -9,6 +9,7 @@ const {
   getAllPosts,
   getPostsByUser,
   createTags,
+  addTagsToPost,
 } = require("./index");
 
 async function dropTables() {
@@ -120,6 +121,30 @@ async function createInitialPosts() {
   }
 }
 
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      "#happy",
+      "#worst-day-ever",
+      "#youcandoanything",
+      "#catmandoeverything",
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
+    throw error;
+  }
+}
+
 async function rebuildDB() {
   try {
     client.connect();
@@ -128,7 +153,8 @@ async function rebuildDB() {
     await createTables();
     await createInitialUsers();
     await createInitialPosts();
-    await createTags();
+
+    await createInitialTags();
   } catch (error) {
     throw error;
   }
